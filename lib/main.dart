e725e9b8b8d2b435a94a5bd492ec8b3edaf88853
ryokuchayurai <ryokuchayurai@gmail.com>
2022-05-26 08:32:54 +0900

    add particle

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'dart:ui';
 
 import 'package:flame/components.dart';
@@ -5,6 +6,7 @@ import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
 import 'package:flame/palette.dart';
+import 'package:flame/particles.dart';
 import 'package:flame/sprite.dart';
 import 'package:flame_audio/audio_pool.dart';
 import 'package:flame_audio/flame_audio.dart';
@@ -18,21 +20,20 @@ import 'package:flutter/material.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
-    theme: ThemeData(
-      primarySwatch: Colors.brown,
-      scaffoldBackgroundColor: Color.fromARGB(0xFF, 0xFE, 0xFE, 0xF9),
-      textTheme: GoogleFonts.sawarabiMinchoTextTheme()),
+      theme: ThemeData(
+          primarySwatch: Colors.brown,
+          scaffoldBackgroundColor: Color.fromARGB(0xFF, 0xFE, 0xFE, 0xF9),
+          textTheme: GoogleFonts.sawarabiMinchoTextTheme()),
       home: MyAppHome()));
 }
 
-class MyAppHome extends StatelessWidget{
+class MyAppHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GameWidget(
       game: TiledGame(),
     );
   }
-
 }
 
 enum Screen {
@@ -40,8 +41,7 @@ enum Screen {
   Test2,
 }
 
-class MyGame extends FlameGame with TapDetector, HasDraggables, KeyboardEvents{
-
+class MyGame extends FlameGame with TapDetector, HasDraggables, KeyboardEvents {
   List<FlameGame> _stack = [];
 
   @override
@@ -50,7 +50,7 @@ class MyGame extends FlameGame with TapDetector, HasDraggables, KeyboardEvents{
   }
 
   Future<void> push(Screen screen) async {
-    switch(screen) {
+    switch (screen) {
       case Screen.Test1:
         _stack.add(TiledGame());
         break;
@@ -61,12 +61,12 @@ class MyGame extends FlameGame with TapDetector, HasDraggables, KeyboardEvents{
     await add(_stack.last);
   }
 
-  Future<void> pop()  async{
+  Future<void> pop() async {
     remove(_stack.last);
     _stack.removeLast();
   }
 
-  Future<void> popAndPush(Screen screen)  async{
+  Future<void> popAndPush(Screen screen) async {
     pop();
     push(screen);
   }
@@ -80,7 +80,8 @@ class MyGame extends FlameGame with TapDetector, HasDraggables, KeyboardEvents{
   }
 }
 
-class TiledGame extends FlameGame with TapDetector, HasDraggables, KeyboardEvents {
+class TiledGame extends FlameGame
+    with TapDetector, HasDraggables, KeyboardEvents {
   late Image coins;
   late final JoystickComponent joystick;
 
@@ -89,7 +90,7 @@ class TiledGame extends FlameGame with TapDetector, HasDraggables, KeyboardEvent
     await super.onLoad();
 
     // camera.zoom = 2;
-    camera.viewport = FixedResolutionViewport(Vector2(375,667));
+    camera.viewport = FixedResolutionViewport(Vector2(375, 667));
 
     final tiledMap = await TiledComponent.load('map.tmx', Vector2.all(16));
     // final tiledMap = await loadTiledComponent('map.tmx', Vector2.all(16));
@@ -176,10 +177,10 @@ class TiledGame extends FlameGame with TapDetector, HasDraggables, KeyboardEvent
     // }
     add(TextComponent(
         text: 'Hello, Flame',
-        textRenderer: TextPaint(style: TextStyle(
-            fontFamily: GoogleFonts.sawarabiMincho().fontFamily,
-            color: Colors.white
-        )))
+        textRenderer: TextPaint(
+            style: TextStyle(
+                fontFamily: GoogleFonts.sawarabiMincho().fontFamily,
+                color: Colors.white)))
       ..x = details.eventPosition.game.x
       ..y = details.eventPosition.game.y);
 
@@ -209,30 +210,158 @@ class TiledGame extends FlameGame with TapDetector, HasDraggables, KeyboardEvent
 
   @override
   KeyEventResult onKeyEvent(
-      RawKeyEvent event,
-      Set<LogicalKeyboardKey> keysPressed,
-      ) {
+    RawKeyEvent event,
+    Set<LogicalKeyboardKey> keysPressed,
+  ) {
     final isKeyDown = event is RawKeyDownEvent;
 
     if (event.logicalKey == LogicalKeyboardKey.keyX) {
       Navigator.of(buildContext!).pop();
-      Navigator.of(buildContext!).push(MaterialPageRoute(
-        builder: (context) {
-          return GameWidget(game: TestGame());
-        }
-      ));
+      Navigator.of(buildContext!).push(MaterialPageRoute(builder: (context) {
+        return GameWidget(game: TestGame());
+      }));
     }
 
     return super.onKeyEvent(event, keysPressed);
   }
 }
 
-class TestGame extends FlameGame {
+class TestGame extends FlameGame with TapDetector {
+  final random = Random();
+  final Tween<double> noise = Tween(begin: -10, end: 10);
+  final ColorTween colorTween =
+      ColorTween(begin: Colors.white, end: Colors.red);
+  late final coins;
+
   @override
   Future<void> onLoad() async {
-    add(TextComponent(text: 'ああああああ', textRenderer: TextPaint(style: TextStyle(
-      fontFamily: GoogleFonts.sawarabiMincho().fontFamily,
-          color: Colors.white
-    ))));
+    coins = await Flame.images.load('coins.png');
+    add(TextComponent(
+        text: 'ああああああ',
+        textRenderer: TextPaint(
+            style: TextStyle(
+                fontFamily: GoogleFonts.sawarabiMincho().fontFamily,
+                color: Colors.white))));
+
+    add(
+      ParticleSystemComponent(
+        position: Vector2(100, 100),
+        particle: Particle.generate(
+          count: 40,
+          lifespan: 5,
+          generator: (i) {
+            return AcceleratedParticle(
+              speed: Vector2(
+                    noise.transform(random.nextDouble()),
+                    noise.transform(random.nextDouble()),
+                  ) *
+                  i.toDouble(),
+              child: CircleParticle(
+                radius: 2,
+                paint: Paint()
+                  ..color = colorTween.transform(random.nextDouble())!,
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  void onTapDown(TapDownInfo info) async {
+    add(ParticleSystemComponent(
+        position: info.eventPosition.game, particle: animationParticle()));
+  }
+
+  Particle fireworkParticle() {
+    // A pallete to paint over the "sky"
+    final paints = [
+      Colors.amber,
+      Colors.amberAccent,
+      Colors.red,
+      Colors.redAccent,
+      Colors.yellow,
+      Colors.yellowAccent,
+      // Adds a nice "lense" tint
+      // to overall effect
+      Colors.blue,
+    ].map((color) => Paint()..color = color).toList();
+
+    return Particle.generate(
+      count: 100,
+      lifespan: 1,
+      generator: (i) {
+        final initialSpeed = randomCellVector2();
+        final deceleration = initialSpeed * -1;
+        final gravity = Vector2(0, 1000);
+
+        return AcceleratedParticle(
+          speed: initialSpeed,
+          acceleration: gravity,
+          // acceleration: deceleration,// + gravity,
+
+          child: ComputedParticle(
+            renderer: (canvas, particle) {
+              final paint = randomElement(paints);
+              // Override the color to dynamically update opacity
+              // paint.color = paint.color.withOpacity(1 - particle.progress);
+
+              canvas.drawCircle(
+                Offset.zero,
+                // Closer to the end of lifespan particles
+                // will turn into larger glaring circles
+                random.nextDouble() * particle.progress > .6
+                    ? random.nextDouble() * (50 * particle.progress)
+                    : 5 + (3 * particle.progress),
+                paint,
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  Vector2 randomCellVector2() {
+    return (Vector2.random() - Vector2.random())..multiply(Vector2(1000, 1000));
+  }
+
+  T randomElement<T>(List<T> list) {
+    return list[random.nextInt(list.length)];
+  }
+
+  Particle animationParticle() {
+    return Particle.generate(
+        count: 100,
+        lifespan: 1,
+        generator: (i) {
+          final initialSpeed = randomCellVector2();
+          final deceleration = initialSpeed * -1;
+          final gravity = Vector2(0, 1000);
+
+          return AcceleratedParticle(
+              speed: initialSpeed,
+              acceleration: gravity,
+              // acceleration: deceleration,// + gravity,
+
+              child: SpriteAnimationParticle(
+                animation: getBoomAnimation(),
+                size: Vector2(20, 20),
+              ));
+        });
+  }
+
+  SpriteAnimation getBoomAnimation() {
+    const columns = 8;
+    const rows = 1;
+    const frames = columns * rows;
+    final spritesheet = SpriteSheet.fromColumnsAndRows(
+      image: coins,
+      columns: columns,
+      rows: rows,
+    );
+    final sprites = List<Sprite>.generate(frames, spritesheet.getSpriteById);
+    return SpriteAnimation.spriteList(sprites, stepTime: 0.2);
   }
 }
